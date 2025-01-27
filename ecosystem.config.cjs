2b88ce352da75ab065ef303b885ae2ed82c5f73b
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-01-27 15:06:41 +0800

    fix ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,11 @@ module.exports = {
   apps: [
     {
       name: "eoffice",
-      port: "2520",
-      exec_mode: "cluster",
-      instances: "max",
       script: "./.output/server/index.mjs",
+      env: {
+        NITRO_PORT: 2520,
+        NODE_ENV: "production",
+      },
     },
   ],
 };
